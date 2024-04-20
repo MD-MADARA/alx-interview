2f936250fd2b226f2552161427919a4d35ef059b
MD-MADARA <mouadotaku9421@gmail.com>
2024-04-20 19:10:52 +0100

    push

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,15 +4,5 @@
 
 def pascal_triangle(n):
     """Triangle"""
-    if n <= 0:
-        return []
-    pas = [[1]]
-    for row_number in range(1, n):
-        row = [1]
-        for j in range(1, row_number):
-            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
-            row.append(element)
-        row.append(1)
-        pas.append(row)
-
-    return pas
+    return [[int(digit) for digit in str(11 ** i)] for i in range(n)] \
+        if n >= 0 else []
